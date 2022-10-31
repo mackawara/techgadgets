8e1b25e0de29fdbeda3bb6c688cbb4b0fcad6115
mackawara <mkawara@outlook.com>
2022-11-01 00:27:21 +0200

    feat :addedpost routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const ejs = require("ejs");
 //const routes = require('./routes');
 require("dotenv").config();
 
@@ -12,19 +13,27 @@ app.set(`views`, __dirname + "/views");
 
 //routes(app);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}!`)
-});
+const PORT = process.env.PORT || 3000;
 
-app.post("./signUp",(req,res)=>{
-    const email=req.body.email
-    res.render("home.ejs",{customer:email})
-})
-app.post("./contactForm",(req,res)=>{
-    const email=req.body.email,name=req.body.name,message=req.body.message
-    res.render("home.ejs",{customer:name})
-})
- app.get("./",(req,res)=>{
-    res.sendFile(__dirname + "/index.html");
- })
+const {signUpRules,contactFormRules,validateForm}=require("./middleware/validation")
+
+app.post("/signUp",signUpRules,validateForm, (req, res) => {
+  console.log("signUp pinged");
+  const email = req.body.email;
+
+  console.log(email);
+  res.render("home.ejs", { customer: email });
+});
+app.post("./contactForm",contactFormRules,validateForm, (req, res) => {
+  const email = req.body.email,
+    name = req.body.name,
+    message = req.body.message;
+  res.render("home.ejs", { customer: name });
+});
+app.get("/", (req, res) => {
+  console.log("pinged");
+  res.sendFile(__dirname + "/generic.html");
+});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
